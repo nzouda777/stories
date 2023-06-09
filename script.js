b973c62b839a6ede1646772ef7dfdc1026d2b1b9
nzouda777 <nzoudarodrigue@gmail.com>
2023-06-09 01:39:14 +0100

    story ui safari update

--- script.js
+++ script.js
@@ -10,51 +10,55 @@ const storiesContainer = document.querySelector('.stories');
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
 
-if (! navigator. userAgent. includes('Chrome') && navigator. userAgent. includes('Safari')) {
+    
 
-    stories.forEach((story) => {
-        story.addEventListener('click', () => {
-          const imageSrc = story.querySelector('video').src;
-          const username = story.querySelector('.username').textContent;
-          popupSafari.style.background = `url(${imageSrc})`
-          video = story.querySelector('video')
-          popupSafari.play()
-          arrowLeft.style.visibility = "hidden"
-          
-          arrowRight.style.visibility = "hidden"
-          popupUsername.textContent = username;
-          popup.style.display = 'flex';
-        });
-      });
+
+
+if (! navigator. userAgent. includes('Chrome') && navigator. userAgent. includes('Safari')) {
+stories.forEach((story) => {
+    story.addEventListener('click', () => {
+      const imageSrc = story.querySelector('video').src;
+      const username = story.querySelector('.username').textContent;
+      popupVideo.src = imageSrc;
+      popupVideo.setAttribute("controls", "true")
+      video = story.querySelector('video')
+      popupVideo.play()
+      arrowLeft.style.visibility = "hidden"
       
-      closeButton.addEventListener('click', () => {
-        popup.style.display = 'none';
-        window.location.reload()
-        arrowLeft.style.visibility = "visible"
-        arrowRight.style.visibility = "visible"
+      arrowRight.style.visibility = "hidden"
+      popupUsername.textContent = username;
+      popup.style.visibility = 'visible';
+    });
+  });
+  
+  closeButton.addEventListener('click', () => {
+    popup.style.visibility = 'hidden';
+    popupVideo.pause()
+    arrowLeft.style.visibility = "visible"
+    arrowRight.style.visibility = "visible"
+  });
+  arrowLeft.addEventListener('click', () => {
+      storiesContainer.scrollBy({
+        left: -200,
+        behavior: 'smooth'
       });
-      arrowLeft.addEventListener('click', () => {
-          storiesContainer.scrollBy({
-            left: -200,
-            behavior: 'smooth'
-          });
-        });
-        
-        arrowRight.addEventListener('click', () => {
-          storiesContainer.scrollBy({
-            left: 200,
-            behavior: 'smooth'
-          });
-        });
-        
-        storiesContainer.addEventListener('scroll', () => {
-          arrowLeft.style.opacity = storiesContainer.scrollLeft > 0 ? 1 : 0;
-          arrowRight.style.opacity =
-            storiesContainer.scrollLeft + storiesContainer.offsetWidth <
-            storiesContainer.scrollWidth
-              ? 1
-              : 0;
-        });
+    });
+    
+    arrowRight.addEventListener('click', () => {
+      storiesContainer.scrollBy({
+        left: 200,
+        behavior: 'smooth'
+      });
+    });
+    
+    storiesContainer.addEventListener('scroll', () => {
+      arrowLeft.style.opacity = storiesContainer.scrollLeft > 0 ? 1 : 0;
+      arrowRight.style.opacity =
+        storiesContainer.scrollLeft + storiesContainer.offsetWidth <
+        storiesContainer.scrollWidth
+          ? 1
+          : 0;
+    });
     
 
 } else {
@@ -103,5 +107,3 @@ if (! navigator. userAgent. includes('Chrome') && navigator. userAgent. includes
         });
       
 }
-
-
